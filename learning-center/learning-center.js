e6087e1f8a299e29849e4acb58271d6ad782eca5
Erica Perry <erica.perry@woodlanddirect.com>
2019-06-04 14:03:56 -0400

    [DEV-12][fc]gallery switching working

--- learning-center/learning-center.js
+++ learning-center/learning-center.js
@@ -5,6 +5,7 @@
 
 // put image url and alt data below, in between the single quotes
 // copy from the first { to the }, to add another object or remove
+// if you don't want the first one to display first, change selected to false and assign the one you want to display to true
 var galleryImages = [
   {
     imageSource: 'https://via.placeholder.com/300',
@@ -12,18 +13,37 @@ var galleryImages = [
     selected: true
   },
   {
-    imageSource: 'https://via.placeholder.com/300',
+    imageSource: 'https://via.placeholder.com/500',
     altTag: 'alt tag text here',
     selected: false
   },
   {
-    imageSource: 'https://via.placeholder.com/300',
+    imageSource: 'https://via.placeholder.com/600',
     altTag: 'alt tag text here',
     selected: false
-  }
-];
+  },
+]
+
+function handleGalleryClick (idx) {
+  var selectedImage = document.getElementsByClassName('gallery-img')
+  var selectedBullet = document.getElementsByClassName('bullet')
+
+  // this finds the previously selected item and makes it false, and removes the active classes for the object
+  galleryImages.find(function (image, index) {
+    if (image.selected === true) {
+      image.selected = false
+      selectedImage[index].classList.remove('display-img')
+      selectedBullet[index].classList.remove('active')
+    }
+  })
+
+  // sets new selected and active objects
+  galleryImages[idx].selected = true
+  selectedImage[idx].classList.add('display-img')
+  selectedBullet[idx].classList.add('active')
+}
 // this function populates gallery
-(function () {
+;(function () {
   var imageContainer = document.getElementById('image-container')
   var buttonContainer = document.getElementById('bullet-container')
   galleryImages.forEach(function (image, idx) {
@@ -46,6 +66,9 @@ var galleryImages = [
       : buttonElement.setAttribute('class', 'bullet')
     buttonElement.setAttribute('aria-label', 'view image number' + (idx + 1))
     buttonElement.setAttribute('index', idx)
+    buttonElement.onclick = function () {
+      handleGalleryClick(idx)
+    }
     buttonContainer.appendChild(buttonElement)
   })
 })()
