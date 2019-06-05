8e98d032493d27217d6787714bfa01e2beac1253
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-05 14:21:26 -0400

    [DEV-24][mf]create NavArrow svg

--- src/sideBar/SideBar.jsx
+++ src/sideBar/SideBar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import NavArrow from './parts/NavArrow'
+
 const SideBarContainer = styled.div`
   background-color: black;
+  opacity: .75;
   height: 100%;
-  width: 100px;
+  width: auto;
   position: absolute;
 `
 const SideBar = () => {
-  return <SideBarContainer />
+  return( <SideBarContainer>
+    <NavArrow/>
+    </SideBarContainer>)
 }
 export default SideBar
