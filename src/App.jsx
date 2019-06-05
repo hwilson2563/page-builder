811ec1f4ed151b90800d07603d409bfaf3f303bf
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-05 13:49:49 -0400

    [DEV-24][nf] add style components and get simple nav on the page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import SideBar from './sideBar/SideBar.jsx'
+function App () {
+  return (
+    <div className='App'>
+      {/* <header className='App-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header> */}
+      <SideBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/sideBar/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+const SideBarContainer = styled.div`
+  background-color: black;
+  height: 100%;
+  width: 100px;
+  position: absolute;
+`
+const SideBar = () => {
+  return <SideBarContainer />
+}
+export default SideBar
