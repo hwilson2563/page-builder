e90669b4a1a29d5efef5ec9afbd4dc06921a9235
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-05 11:32:47 -0400

    [DEV-13][sx]clean up with comments and remove console logs

--- learning-center/learning-center.js
+++ learning-center/learning-center.js
@@ -1,21 +1,25 @@
 function readMore () {
-  var readMoreContainer = document.getElementsByClassName('read-more-container')
-  if (readMoreContainer && readMoreContainer.length > 0) {
-    for (var x = 0; x < readMoreContainer.length; x++) {
-      var paragraghs = readMoreContainer[x].children
-      var paragraphsHeight = 0
-      for (var y = 0; y < paragraghs.length; y++) {
-        var heightIncludesMargin = paragraghs[y].offsetHeight + 20
-        paragraphsHeight += heightIncludesMargin
-      }
-      if (paragraphsHeight >= 300) {
-        var parentDiv = readMoreContainer[x].parentElement
-        parentDiv.classList.add('read')
+  var inTabletView = window.innerWidth < 1020 && window.innerWidth > 740
+  if (inTabletView) {
+    var readMoreContainer = document.getElementsByClassName('read-more-container')
+    if (readMoreContainer && readMoreContainer.length > 0) {
+      for (var x = 0; x < readMoreContainer.length; x++) {
+        var paragraghs = readMoreContainer[x].children
+        var paragraphsHeight = 0
+        for (var y = 0; y < paragraghs.length; y++) {
+          var heightIncludesMargin = paragraghs[y].offsetHeight + 20
+          paragraphsHeight += heightIncludesMargin
+        }
+        if (paragraphsHeight >= 300) {
+          var parentDiv = readMoreContainer[x].parentElement
+          parentDiv.classList.add('read')
+        }
       }
     }
   }
 }
-readMore()
+window.addEventListener('onresize', readMore())
+//  start of onclick function attached to show more button
 function showMore (button) {
   var parentElement = button.parentElement
   parentElement.classList.toggle('more')
