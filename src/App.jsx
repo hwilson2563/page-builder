70be1c9ea7cdc2b4b5e05e9bf1fae8b29f68795e
Ted Weiss <ted.weiss@woodlanddirect.com>
2019-06-05 14:26:52 -0400

    [DEV-23][fc] change images on breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import TemplatesPreview from './TemplatesPreview'
+import { determineScreen } from './utils'
 
 const App = () => {
-  return <TemplatesPreview />
+  const [screen, setScreen] = useState()
+
+  useEffect(() => {
+    const updateScreen = () => {
+      let width = window.innerWidth
+      let screen = determineScreen(width)
+      setScreen(screen)
+    }
+    // call function for initial run
+    updateScreen()
+    // add event listener
+    window.addEventListener('resize', updateScreen)
+    return () => window.removeEventListener('resize', updateScreen)
+  }, []) // Empty array ensures that effect is only run on mount and unmount
+
+  return <TemplatesPreview screen={screen} />
 }
 
 export default App
--- src/TemplatesPreview.jsx
+++ src/TemplatesPreview.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const TemplatesPreview = () => {
+const TemplatesPreview = props => {
+  const { screen } = props
+
   return (
     <>
       <img
         style={{ width: '100%' }}
-        src={'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/header-footer/header-desktop.png'}
+        src={'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/header-footer/header-' + screen + '.png'}
         alt={'Header'}
       />
       <img
         style={{ width: '100%' }}
-        src={'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/header-footer/footer-desktop.png'}
+        src={'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/header-footer/footer-' + screen + '.png'}
         alt={'Footer'}
       />
     </>
