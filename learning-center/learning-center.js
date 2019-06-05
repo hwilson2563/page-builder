df5afe56b25e5217657e686b23700d06625c8226
Erica Perry <erica.perry@woodlanddirect.com>
2019-06-05 11:00:46 -0400

    [DEV-12][nf, fc]render only one gallery

--- learning-center/learning-center.js
+++ learning-center/learning-center.js
@@ -6,30 +6,15 @@
 // put image url and alt data below, in between the single quotes
 // copy from the first { to the }, to add another object or remove
 // if you don't want the first one to display first, change selected to false and assign the one you want to display to true
-var galleryImages = [
-  {
-    imageSource: 'https://via.placeholder.com/700',
-    altTag: 'alt tag text here',
-    selected: true
-  },
-  {
-    imageSource: 'https://via.placeholder.com/500',
-    altTag: 'alt tag text here',
-    selected: false
-  },
-  {
-    imageSource: 'https://via.placeholder.com/600',
-    altTag: 'alt tag text here',
-    selected: false
-  },
-]
+var galleryImages = JSON.parse(document.getElementById('galleries').innerHTML)
+var selectedGallery = null
 
-function handleGalleryClick (idx) {
+function handleGalleryClick (idx, galleryName) {
   var selectedImage = document.getElementsByClassName('gallery-img')
   var selectedBullet = document.getElementsByClassName('bullet')
 
   // this finds the previously selected item and makes it false, and removes the active classes for the object
-  galleryImages.forEach(function (image, index) {
+  galleryImages[galleryName].forEach(function (image, index) {
     if (image.selected === true) {
       image.selected = false
       selectedImage[index].classList.remove('display-img')
@@ -38,16 +23,18 @@ function handleGalleryClick (idx) {
   })
 
   // sets new selected and active objects
-  galleryImages[idx].selected = true
+  galleryImages[galleryName][idx].selected = true
   selectedImage[idx].classList.add('display-img')
   selectedBullet[idx].classList.add('active')
 }
-// this function populates gallery
-;(function () {
+
+function buildGallery (gallery) {
   var imageContainer = document.getElementById('image-container')
   var buttonContainer = document.getElementById('bullet-container')
-  galleryImages.forEach(function (image, idx) {
-    // this builds the image
+
+  // this loops through each gallery in the JSON
+  gallery.forEach(function (image, idx) {
+    // this builds the images
     var imageTag = document.createElement('img')
     imageTag.setAttribute('id', 'img' + idx)
     image.selected
@@ -65,8 +52,21 @@ function handleGalleryClick (idx) {
       : buttonElement.setAttribute('class', 'bullet')
     buttonElement.setAttribute('aria-label', 'view image number' + (idx + 1))
     buttonElement.onclick = function () {
-      handleGalleryClick(idx)
+      handleGalleryClick(idx, selectedGallery)
     }
     buttonContainer.appendChild(buttonElement)
+  })
+}
+
+// this initially function populates gallery on load
+;(function () {
+  var objectProperyNames = Object.getOwnPropertyNames(galleryImages)
+  // this loops through JSON
+  Object.values(galleryImages).forEach(function (gallery, index) {
+    if (index === 0) {
+      selectedGallery = objectProperyNames[index]
+      // this loops through each gallery in the JSON
+      buildGallery(gallery)
+    }
   })
 })()
