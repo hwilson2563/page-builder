f9355255bc8b225f57321244235858b3e1a10f28
Erica Perry <erica.perry@woodlanddirect.com>
2019-06-07 12:10:47 -0400

    [DEV-22][nf]icon file

---
+++ src/components/parts/ExportIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Export = styled.svg`
+  width: 100%;
+`
+
+const ExportIcon = () => {
+  return <Export>Hello</Export>
+}
+
+export default ExportIcon
