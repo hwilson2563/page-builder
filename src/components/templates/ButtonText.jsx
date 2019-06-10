7702db61a0861e917f99141429849cb47cdece89
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-10 12:06:50 -0400

    [DEV-38][nf]make button text template into jsx

--- src/components/templates/ButtonText.jsx
+++ src/components/templates/ButtonText.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const ButtonText = () => {
-  return <div>Button Text Template</div>
+  return (
+  // <!-- START BUTTON TEXT TEMPLATE -->
+    <div id='button-text-template' class='button-text-template'>
+      <p class='button-text-template-text'>place a call to action of some kind here</p>
+      <a class='button-link' href='' aria-label=''>
+        <button class='template-button'>press this button</button>
+      </a>
+    </div>
+  // <!-- END BUTTON TEXT TEMPLATE -->
+  )
 }
 
 export default ButtonText
