5d4c8c41c070ca7191cfab44307ccb5f31cbe47b
Erica Perry <erica.perry@woodlanddirect.com>
2019-06-10 13:41:08 -0400

    [DEV-31]import dev modal stuff

---
+++ src/components/modal/formEntryStyles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+export const EntryContainer = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+export const Label = styled.label`
+  position: absolute;
+  font-family: ${props => props.theme.fontBody};
+  font-size: ${props => (props.selected ? '14px' : '14px')};
+  font-weight: normal !Important; /* this is to override core */
+  top: 20px;
+  left: 10px;
+  cursor: text;
+  pointer-events: none;
+  color: ${props => (props.selected ? props.theme.accentSecondary : '#999')};
+  ${props =>
+    props.selected
+      ? `
+    pointer-events: none;
+    transform: translateZ(0) scale(1);
+    transition: all .25s ease-in-out;
+    transform-origin: left top 0;
+    transform: translate3d(0,-9px,0) scale(.8);`
+      : `
+    transform-origin: 0;
+    transition: all .25s ease-in-out;
+    transform: translate3d(0,0,0) scale(1);`}
+  ${props =>
+    props.value && props.value.length >= 1
+      ? `
+    pointer-events: none;
+    transform: translateZ(0) scale(1);
+    transition: all .25s ease-in-out;
+    transform-origin: left top 0;
+    transform: translate3d(0,-9px,0) scale(.8);`
+      : ' '}
+`
+
+let entryStyles = props => {
+  let entryStyle = `
+    box-sizing: border-box;
+    font-family: ${props.theme.fontBody};
+    font-size: 14px;
+    line-height: 20px;
+    margin-bottom: 10px;
+    padding:25px 5px 10px 15px;
+    outline: none;
+    border: ${props.error !== false ? `1px solid ${props.theme.background}` : '1px solid red'};
+    width: 100%; /* width: 100% for ie */
+    width:fill-available;
+  `
+  return entryStyle
+}
+
+export const Input = styled.input`
+  ${props => entryStyles(props)}
+`
---
+++ src/components/modals/BannerModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import FormEntry from '../modal/FormEntry'
+const BannerModal = props => {
+  const { updateFormData } = props
+  return (
+    <>
+      <FormEntry type={'input'} label={'Image'} name={'image'} error={null} updateFormData={updateFormData} required />
+      <FormEntry type={'input'} label={'Title'} name={'title'} error={null} updateFormData={updateFormData} required />
+      <FormEntry
+        type={'input'}
+        label={'Subtitle'}
+        name={'subtitle'}
+        error={null}
+        updateFormData={updateFormData}
+        required
+      />
+    </>
+  )
+}
+export default BannerModal
