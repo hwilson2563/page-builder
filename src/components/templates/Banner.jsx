cacdd307d06f528af769f8ab1f2cb33ce267f9ab
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-10 12:05:07 -0400

    [DEV-38][nf]make banner template into jsx

--- src/components/templates/Banner.jsx
+++ src/components/templates/Banner.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const Banner = () => {
-  return <div>Banner Template</div>
+  return (
+    // <!-- START BANNER TEMPLATE -->
+    // <!-- for a different background image, replace the link in quotes -->
+    <div
+      style={{
+        backgroundImage: `url('https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg')`
+      }}
+      id='banner-temp'>
+      <div className='banner-text'>
+        <h1>place title here</h1>
+        <p>place subheading text here</p>
+      </div>
+    </div>
+    // <!-- END BANNER TEMPLATE -->
+  )
 }
 
 export default Banner
