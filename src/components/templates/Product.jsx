fa35a52a60bef2ebf980459a8099133880abdf65
lauren.kolpasky <lauren.kolpasky@woodlanddirect.com>
2019-06-11 08:05:51 -0400

    [DEV-38] added 4 more templates, onclick doesnt work with html

--- src/components/templates/Product.jsx
+++ src/components/templates/Product.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
 
 const Product = () => {
-  return <div>Product Template</div>
+  return (
+    // <!-- START PRODUCT LIST TEMPLATE -->
+    <div id='product-list-temp'>
+      <div className='product-container'>
+        {/* <!-- add/remove products as needed -->
+    <!-- single product starts here (for copying and pasting) --> */}
+        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+          <h4>Place Product Name Here</h4>
+          <p>Starting at</p>
+          <p>$ price here</p>
+        </div>
+        {/* <!-- single product ends here (copy between comments) -->
+    <!-- product begins -->
+    <!-- put product link in single quotes where the home page link is --> */}
+        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+          <h4>Place Product Name Here</h4>
+          <p>Starting at</p>
+          <p>$ price here</p>
+        </div>
+        {/* <!-- product ends -->
+    <!-- product begins --> */}
+        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+          <h4>Place Product Name Here</h4>
+          <p>Starting at</p>
+          <p>$ price here</p>
+        </div>
+        {/* <!-- product ends -->
+    <!-- product begins --> */}
+        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+          <h4>Place Product Name Here</h4>
+          <p>Starting at</p>
+          <p>$ price here</p>
+        </div>
+        {/* <!-- product ends -->
+    <!-- product begins --> */}
+        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+          <h4>Place REALLY really LONG Product Name Here</h4>
+          <p>Starting at</p>
+          <p>$ price here</p>
+        </div>
+        {/* <!-- product ends -->
+    <!-- COPY AND PASTE ANY OTHER PRODUCTS BEFORE THIS COMMENT --> */}
+      </div>
+    </div>
+    /* <!-- END PRODUCT LIST TEMPLATE --> */
+  )
 }
 
 export default Product
--- src/components/templates/SingleButton.jsx
+++ src/components/templates/SingleButton.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const SingleButton = () => {
-  return <div>Single Button Template</div>
+  return (
+  // <!-- START SINGLE BUTTON -->
+
+    <div id='button-text-template' class='button-text-template'>
+      <a class='button-link' href='' aria-label=''>
+        <button class='template-button'>press this button</button>
+      </a>
+    </div>
+
+  // <!-- END SINGLE BUTTON -->
+  )
 }
 
 export default SingleButton
