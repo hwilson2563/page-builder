dbd6c079afdbc9a0731e15bc8baa2c774b2d4c04
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-12 10:45:50 -0400

    [DEV-38][sx]fix class and frameboarder

--- src/components/templates/Video.jsx
+++ src/components/templates/Video.jsx
@@ -4,9 +4,9 @@ const Video = () => {
   return (
     // <!-- START VIDEO TEMPLATE -->
     <div id='video-template'>
-      <div class='iframe-container'>
+      <div className='iframe-container'>
         {/* <!-- FOR LINK: if using a youtube link, just replace the 'watch?v=' with 'embed/' from whichever youtube video you want: so https://www.youtube.com/watch?v=ZY3J3Y_OU0w becomes what you see below --> */}
-        <iframe src='https://www.youtube.com/embed/ZY3J3Y_OU0w' frameborder='0' width='560' height='315' title='video' />
+        <iframe src='https://www.youtube.com/embed/ZY3J3Y_OU0w' frameBorder='0' width='560' height='315' title='video' />
       </div>
     </div>
     // <!-- END VIDEO TEMPLATE -->
