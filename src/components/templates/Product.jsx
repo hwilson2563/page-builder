71467b490ebf25887198059bee5e64689d4f58bd
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-12 08:56:49 -0400

    [DEV-38][fc]change divs to a tags

--- src/components/templates/Product.jsx
+++ src/components/templates/Product.jsx
@@ -3,49 +3,49 @@ import React from 'react'
 const Product = () => {
   return (
     // <!-- START PRODUCT LIST TEMPLATE -->
-    <div id='product-list-temp'>
-      <div className='product-container'>
+    <div id={'product-list-temp'}>
+      <div className={'product-container'}>
         {/* <!-- add/remove products as needed -->
     <!-- single product starts here (for copying and pasting) --> */}
-        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+        <a className={'single-product'} href={'https://www.woodlanddirect.com'} target={'__blank'}>
           <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
           <h4>Place Product Name Here</h4>
           <p>Starting at</p>
           <p>$ price here</p>
-        </div>
+        </a>
         {/* <!-- single product ends here (copy between comments) -->
     <!-- product begins -->
     <!-- put product link in single quotes where the home page link is --> */}
-        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+        <a className={'single-product'} href={'https://www.woodlanddirect.com'} target={'__blank'}>
           <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
           <h4>Place Product Name Here</h4>
           <p>Starting at</p>
           <p>$ price here</p>
-        </div>
+        </a>
         {/* <!-- product ends -->
     <!-- product begins --> */}
-        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+        <a className={'single-product'} href={'https://www.woodlanddirect.com'} target={'__blank'}>
           <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
           <h4>Place Product Name Here</h4>
           <p>Starting at</p>
           <p>$ price here</p>
-        </div>
+        </a>
         {/* <!-- product ends -->
     <!-- product begins --> */}
-        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+        <a className={'single-product'} href={'https://www.woodlanddirect.com'} target={'__blank'}>
           <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
           <h4>Place Product Name Here</h4>
           <p>Starting at</p>
           <p>$ price here</p>
-        </div>
+        </a>
         {/* <!-- product ends -->
     <!-- product begins --> */}
-        <div className='single-product' onClick={() => window.open('https://www.woodlanddirect.com', '_blank')}>
+        <a className={'single-product'} href={'https://www.woodlanddirect.com'} target={'__blank'}>
           <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
           <h4>Place REALLY really LONG Product Name Here</h4>
           <p>Starting at</p>
           <p>$ price here</p>
-        </div>
+        </a>
         {/* <!-- product ends -->
     <!-- COPY AND PASTE ANY OTHER PRODUCTS BEFORE THIS COMMENT --> */}
       </div>
