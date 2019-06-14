0eca6d2b43fdb93896b73398fd482b29110c63a8
Ted Weiss <ted.weiss@woodlanddirect.com>
2019-06-14 13:49:11 -0400

    [DEV-49][fc] pass data props to test only template

--- src/components/templates/TextOnly.jsx
+++ src/components/templates/TextOnly.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-const TextOnly = () => {
+const TextOnly = props => {
+  const { templateData } = props
+  let backgroundDark = templateData.backgroundDark ? 'background-dark' : ''
+  let addPadding = templateData.addPadding ? 'add-padding' : 'padding'
+
+  let title = templateData.title ? templateData.title : 'place an amazing section title here'
+  let paragraph = templateData.paragraph ? templateData.paragraph : 'place paragraph text here'
   return (
     // <!-- START OF TEXT ONLY TEMPLATE -->
-    <div id='text-only-temp'>
-      <h3>place an amazing section title here</h3>
+    <div id='text-only-temp' className={backgroundDark + ' ' + addPadding}>
+      <h3>{title}</h3>
       {/* <!-- start paragraphs here -->
   <!-- add/remove p tags as needed--> */}
-      <p>place paragraph text here</p>
-      <p>place paragraph text here</p>
-      <p>place paragraph text here</p>
+      <p>{paragraph}</p>
       {/* <!-- end paragraphs here --> */}
     </div>
     // <!-- END OF TEXT ONLY TEMPLATE -->
