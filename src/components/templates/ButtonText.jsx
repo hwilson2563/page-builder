1d8f5c0790da131f4c8bea2f43fcf2576098d605
lauren.kolpasky <lauren.kolpasky@woodlanddirect.com>
2019-06-14 11:57:26 -0400

    [DEV-49][fc] passed props into buttonText

--- src/components/templates/ButtonText.jsx
+++ src/components/templates/ButtonText.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const ButtonText = () => {
+const ButtonText = props => {
+  const { templateData } = props
+
+  let backgroundDark = templateData.backgroundDark ? 'background-dark' : ''
+  let addPadding = templateData.addPadding ? 'add-padding' : ''
+  let callToAction = templateData.callToAction ? templateData.callToAction : 'Place a call to action text here'
+  let href = templateData.href ? templateData.href : 'place link here'
+  let ariaLabel = templateData.ariaLabel ? templateData.ariaLabel : ''
+  let buttonText = templateData.buttonText ? templateData.buttonText : 'press this button'
+
   return (
   // <!-- START BUTTON TEXT TEMPLATE -->
-    <div id='button-text-template' className='button-text-template'>
-      <p className='button-text-template-text'>place a call to action of some kind here</p>
-      <a className='button-link' href='place link here' aria-label=''>
-        <button className='template-button'>press this button</button>
+    <div id='button-text-template' className={'button-text-template ' + backgroundDark + ' ' + addPadding}>
+      <p className='button-text-template-text'>{callToAction}</p>
+      <a className='button-link' href={href} aria-label={ariaLabel}>
+        <button className='template-button'>{buttonText}</button>
       </a>
     </div>
   // <!-- END BUTTON TEXT TEMPLATE -->
