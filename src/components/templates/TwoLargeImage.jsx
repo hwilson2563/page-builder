1bb2c1f429075cc2af7dbc555b4485e9efeb754d
Ted Weiss <ted.weiss@woodlanddirect.com>
2019-06-14 13:11:47 -0400

    [DEV-49][fc] pass data props to two large images template

--- src/components/templates/TwoLargeImage.jsx
+++ src/components/templates/TwoLargeImage.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
 
-const TwoLargeImage = () => {
+const TwoLargeImage = props => {
+  const { templateData } = props
+  let backgroundDark = templateData.backgroundDark ? 'background-dark' : ''
+  let addPadding = templateData.addPadding ? 'add-padding' : 'padding'
+
+  let title = templateData.title ? templateData.title : 'Place section title here'
+  let image1 = templateData.image1
+    ? templateData.image1
+    : 'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg'
+  let alt1 = templateData.alt1 ? templateData.alt1 : ''
+  let subheader1 = templateData.subheader1 ? templateData.subheader1 : 'place info item title here'
+  let body1 = templateData.body1 ? templateData.body1 : 'put paragraph text here'
+
+  let image2 = templateData.image2
+    ? templateData.image2
+    : 'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg'
+  let alt2 = templateData.alt2 ? templateData.alt2 : ''
+  let subheader2 = templateData.subheader2 ? templateData.subheader2 : 'place info item title here'
+  let body2 = templateData.body2 ? templateData.body2 : 'put paragraph text here'
+
   return (
     // <!-- START TWO LARGE IMAGE TEMPLATE -->
     // <!-- make sure the images you choose are the same width/height, otherwise it will look weird -->
-    <div id='two-large-image-temp'>
-      <h3>Place Section Title Here</h3>
+    <div id='two-large-image-temp' className={backgroundDark + ' ' + addPadding}>
+      <h3>{title}</h3>
       <div className='section-cont'>
         <div className='section-one'>
-          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
-          <h4>Place Info Item Title Here</h4>
+          <img src={image1} alt={alt1} />
+          <h4>{subheader1}</h4>
           {/* <!-- add/remove p tags to add/remove paragraphs as needed --> */}
-          <p>place paragraph text here</p>
-          <p>place paragraph text here</p>
-          <p>place paragraph text here</p>
+          <p>{body1}</p>
         </div>
         <div className='section-two'>
-          <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
-          <h4>Place Info Item Title Here</h4>
+          <img src={image2} alt={alt2} />
+          <h4>{subheader2}</h4>
           {/* <!-- add/remove p tags to add/remove paragraphs as needed --> */}
-          <p>place paragraph text here</p>
-          <p>place paragraph text here</p>
-          <p>place paragraph text here</p>
+          <p>{body2}</p>
         </div>
       </div>
     </div>
