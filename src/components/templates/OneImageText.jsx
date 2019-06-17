c6d24f95832c59c88bb48d010d94d6aa5f5d7f35
lauren.kolpasky <lauren.kolpasky@woodlanddirect.com>
2019-06-17 10:07:44 -0400

    [DEV-49][fc] added props to one image text template

--- src/components/templates/OneImageText.jsx
+++ src/components/templates/OneImageText.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
-const OneImageText = () => {
+const OneImageText = props => {
+  const { templateData } = props
+
+  let backgroundDark = templateData.backgroundDark ? 'background-dark' : ''
+  let addPadding = templateData.addPadding ? 'add-padding' : 'padding'
+
+  let image = templateData.image ? templateData.image.value : 'https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg'
+  let imageAltText = templateData.imageAltText ? templateData.imageAltText.value : ''
+  let title = templateData.title ? templateData.title.value : 'Place Title Here'
+  let paragraph = templateData.paragraph ? templateData.paragraph.value : 'place title here'
+
   return (
     // <!-- START ONE IMAGE-TEXT-TEMPLATE HERE-->
-    <div id='one-image-text'>
-      <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/grey-img-icon.svg' alt='' />
+    <div id='one-image-text' className={backgroundDark + ' ' + addPadding}>
+      <img src={image} alt={imageAltText} />
       <div className='text-container'>
-        <h3>place an amazing section title here</h3>
+        <h3>{title}</h3>
         {/* <!-- you can add/remove the p tags (each one represents a paragraph break) -->
     <!-- start p tags --> */}
-        <p>place paragraph text here</p>
-        <p>place paragraph text here</p>
-        <p>place paragraph text here</p>
+        <p>{paragraph}</p>
         {/* <!-- end p tags --> */}
       </div>
     </div>
