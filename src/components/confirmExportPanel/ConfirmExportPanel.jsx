20c47cb3329fb608632e2e5a8528a358e60723bb
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-20 15:48:45 -0400

    [DEV-29][fc]fix id being undefined

--- src/components/confirmExportPanel/ConfirmExportPanel.jsx
+++ src/components/confirmExportPanel/ConfirmExportPanel.jsx
@@ -96,9 +96,11 @@ const ConfirmExportPanel = props => {
     let templates
     if (selectedTemplates.length) {
       templates = ReactDOMServer.renderToStaticMarkup(<CssLink />)
-      selectedTemplates.map(template => {
+      selectedTemplates.map((template, idx) => {
         let Template = template.component
-        return (templates += ReactDOMServer.renderToStaticMarkup(<Template templateData={template.data} />))
+        return (templates += ReactDOMServer.renderToStaticMarkup(
+          <Template templateData={template.data} id={template.id} order={idx + 1} />
+        ))
       })
       templates += ReactDOMServer.renderToStaticMarkup(<JsLink />)
     }
