db0a17884d2825f6b383b4738c6c6d3de3279df0
lauren.kolpasky <lauren.kolpasky@woodlanddirect.com>
2019-06-26 15:25:45 -0400

    [DEV-44][fc] deleted unused styled component

--- src/components/modals/TwoSmallImgModal.jsx
+++ src/components/modals/TwoSmallImgModal.jsx
@@ -12,28 +12,6 @@ const ButtonContainer = styled.div`
   justify-content: center;
   flex-direction: row;
 `
-const StyledButton = styled.button`
-  margin: 5px;
-  font-size: 18px;
-  font-weight: 700;
-  height: 20px;
-  outline: none;
-  height: 45px;
-  width: 100%;
-  border: 2px solid ${props => props.theme.backgroundAccent};
-  background-color: white;
-  font-family: ${props => props.theme.fontBody};
-  text-align: center;
-  text-transform: uppercase;
-  color: ${props => props.theme.mainPrimary};
-  border-radius: 3px;
-  margin-bottom: 40px;
-  transition: 0.3s ease-in-out;
-  :hover {
-    cursor: pointer;
-    background-color: ${props => props.theme.backgroundAccent};
-  }
-`
 const Section = styled.div`
   width: 100%;
   padding: 10px;
