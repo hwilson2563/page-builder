a7c4a23bcb1e404f8e577a43c31051ed6a8117ea
Hilary Matusiak <hilary.wilson@woodlanddirect.com>
2019-06-28 12:24:31 -0400

    [Dev-66]test

--- src/components/modal/FormEntry.jsx
+++ src/components/modal/FormEntry.jsx
@@ -7,7 +7,7 @@ import { EntryContainer, Input, Label, TextArea, ErrorMessage } from './formEntr
 // import { doValidation } from '../../../../globals/services/validation'
 
 const FormEntry = props => {
-  const { name, required, type, group, updateFormData, textArea, checked } = props
+  const { name, required, type, group, updateFormData, textArea } = props
   let storedValue = props.value
   let storedError = props.error
   let label = props.required ? props.label : props.label + ' (Optional)'
@@ -15,9 +15,6 @@ const FormEntry = props => {
   const [selected, setSelected] = useState(false)
   const [noError, setNoError] = useState(storedError)
   const [errorMessage, setErrorMessage] = useState('')
-
-  console.log(inputValue)
-
   useEffect(
     () => {
       // update useState as prop type updates
@@ -52,13 +49,8 @@ const FormEntry = props => {
       }
     }
   }
-  let checkedValue = type === 'radio' ? checked : inputValue
   const handleChange = target => {
     let newValue
-    if (type === 'radio') {
-      checkedValue = true
-      newValue = target.value
-    }
     if (type === 'checkbox') {
       newValue = target.checked
     } else {
@@ -67,10 +59,9 @@ const FormEntry = props => {
     setValue(newValue)
   }
   const classGroup = group ? 'groups ' + group : 'input'
-  
   return (
     <EntryContainer className={'entry-container ' + name} label={label}>
-      <Label className={'entry-label'} type={type === 'radio' ? 'checkbox' : type} label={label} selected={selected} value={inputValue}>
+      <Label className={'entry-label'} type={type} label={label} selected={selected} value={inputValue}>
         {label}
       </Label>
       {textArea ? (
@@ -100,7 +91,7 @@ const FormEntry = props => {
             setNoError(true)
             setSelected(true)
           }}
-          checked={checkedValue}
+          checked={type === 'checkbox' ? inputValue : ''}
           type={type}
           name={name}
           required={required}
--- src/components/modals/QuotesModal.jsx
+++ src/components/modals/QuotesModal.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
+<<<<<<< HEAD
 import styled from 'styled-components'
 
+=======
+>>>>>>> parent of b9e40a6... [DEV-63][fc] have form almost fixed for quote modal, radio buttons not updating properly
 import { PropTypes } from 'prop-types'
 import FormEntry from '../modal/FormEntry'
 
-const OptionTitle = styled.div`
-  width: 100%;
-`
-const ImageOption = styled.div`
-  width: 100%;
-  display: flex;
-  height: 32px;
-  align-items: center;
-  padding: 5px 0;
-`
-
 const QuotesModal = props => {
   const { updateFormData, data } = props
+<<<<<<< HEAD
   console.log(data)
+=======
+>>>>>>> parent of b9e40a6... [DEV-63][fc] have form almost fixed for quote modal, radio buttons not updating properly
   return (
     <>
       <FormEntry
@@ -36,6 +31,7 @@ const QuotesModal = props => {
         updateFormData={updateFormData}
         value={data['addPadding'] ? data['addPadding'].value : false}
       />
+<<<<<<< HEAD
       <OptionTitle>Pick Image </OptionTitle>
       <ImageOption>
         <img src='https://dev.woodlanddirect.com/learningcenter/pagebuilder+/svgs/pro-tip-icon.svg' alt='' />
@@ -87,6 +83,70 @@ const QuotesModal = props => {
         updateFormData={updateFormData}
         value={data['bodyText'] ? data['bodyText'].value : ''}
         required
+=======
+      <div>Pick Multiple Quote Templates </div>
+      <FormEntry
+        type={'checkbox'}
+        label={'FAQ Layout'}
+        name={'faqLayout'}
+        error={null}
+        updateFormData={updateFormData}
+        value={data['faqLayout'] ? data['faqLayout'].value : false}
+      />
+      <FormEntry
+        type={'checkbox'}
+        label={'Pro Tips Layout'}
+        name={'proTipLayout'}
+        error={null}
+        updateFormData={updateFormData}
+        value={data['proTipLayout'] ? data['proTipLayout'].value : false}
+      />
+      <FormEntry
+        type={'checkbox'}
+        label={'Customer Feedback Layout'}
+        name={'feedBackLayout'}
+        error={null}
+        updateFormData={updateFormData}
+        value={data['feedBackLayout'] ? data['feedBackLayout'].value : false}
+      />
+      <div>FAQ Form</div>
+      <FormEntry
+        type={'input'}
+        label={'FAQ - Question'}
+        name={'title'}
+        error={data['title'] ? data['title'].error : null}
+        value={data['title'] ? data['title'].value : ''}
+        updateFormData={updateFormData}
+      />
+      <FormEntry
+        textArea
+        type={'text'}
+        label={'FAQ - Answer'}
+        name={'answer'}
+        error={data['answer'] ? data['answer'].error : null}
+        value={data['answer'] ? data['answer'].value : ''}
+        updateFormData={updateFormData}
+      />
+      <div>Pro Form</div>
+      <FormEntry
+        textArea
+        type={'text'}
+        label={'Pro Tip'}
+        name={'proTip'}
+        error={data['proTip'] ? data['proTip'].error : null}
+        value={data['proTip'] ? data['proTip'].value : ''}
+        updateFormData={updateFormData}
+      />
+      <div>Customer Feedback Form</div>
+      <FormEntry
+        textArea
+        type={'text'}
+        label={'Customer Feedback'}
+        name={'customerFeedback'}
+        error={data['customerFeedback'] ? data['customerFeedback'].error : null}
+        value={data['customerFeedback'] ? data['customerFeedback'].value : ''}
+        updateFormData={updateFormData}
+>>>>>>> parent of b9e40a6... [DEV-63][fc] have form almost fixed for quote modal, radio buttons not updating properly
       />
     </>
   )
